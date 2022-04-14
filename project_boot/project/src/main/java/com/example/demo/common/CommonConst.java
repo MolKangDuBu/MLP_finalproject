f33b29a2d57ab14eeb0c8148388932e04e88c8a5
hwang <syhomi@naver.com>
2022-04-14 13:26:43 +0900

    aa

--- project_boot/project/src/main/java/com/example/demo/common/CommonConst.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.common;
-
-public class CommonConst {
-	
-	 
-	public static final String FilePath="upload";
-	public static final String sourceUrl = "http://127.0.0.1:9090/myhome";
-}
--- project_boot/project/src/main/java/com/example/demo/house/HouseService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.house;
-
-import java.util.List;
-
-public interface HouseService {
-	List<HouseDto> getList(HouseDto dto);
-	void insert(HouseDto dto);
-	void image_insert(HouseImageDto dto);
-}
